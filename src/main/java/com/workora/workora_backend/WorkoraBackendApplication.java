43ad8ef2dbd3cb6118d2be2d730f8f323f11eb1b
Naveen Kushwaha <naveenkushwah024@gmail.com>
2026-03-11 18:41:29 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/workora/workora_backend/WorkoraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.workora.workora_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workora/workora_backend/WorkoraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workora.workora_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
